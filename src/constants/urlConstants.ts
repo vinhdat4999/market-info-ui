e987fbea0739b52c4e4cf56d788f3d4c259e8519
Dat Nguyen <vinhdat4999@gmail.com>
2025-04-18 00:50:37 +0700

    update

--- src/constants/urlConstants.ts
+++ src/constants/urlConstants.ts
@@ -1,1 +1,2 @@
 export const MARKET_INFO_URL = "http://192.168.1.35:3000/api/v1/gold-info?goldSource=SJC,DOJI";
+// export const MARKET_INFO_URL = "https://market-info.onrender.com/api/v1/market-info?goldSource=SJC,DOJI";
