0d0b39540244864f83f9f51712798bae93620e6a
Dat Nguyen <vinhdat4999@gmail.com>
2025-05-02 18:52:17 +0700

    update

--- src/constants/common.ts
+++ src/constants/common.ts
@@ -1,3 +1,4 @@
 export const SJC_1L_DATA = "Vàng SJC 1L, 10L, 1KG";
 export const SJC_RING_DATA = "Vàng nhẫn SJC 99,99% 1 chỉ, 2 chỉ, 5 chỉ";
-export const DOJI_RING_DATA = "Nhẫn Tròn 9999 Hưng Thịnh Vượng";
+// export const DOJI_RING_DATA = "Nhẫn Tròn 9999 Hưng Thịnh Vượng";
+export const DOJI_RING_DATA = "nhanhung1chi";
